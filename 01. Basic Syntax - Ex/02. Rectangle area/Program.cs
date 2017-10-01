3bf562816eca7511d5033b42b5c53b73f52bd08f
ddyulgerov <lotto.b3@gmail.com>
2017-10-01 20:40:39 +0300

    Upload 01.Basi Syntax exerceise

---
+++ 01. Basic Syntax - Ex/02. Rectangle area/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2.Rectangle_area
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double width = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+            double area = width * height;
+            Console.WriteLine($"{area:f2}");
+
+        }
+    }
+}
---
+++ 01. Basic Syntax - Ex/04. Beverage Labels/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4.Beverage_Labels
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string name = Console.ReadLine();
+            int volume = int.Parse(Console.ReadLine());
+            int energy = int.Parse(Console.ReadLine());
+            int sugar = int.Parse(Console.ReadLine());
+            double volumePer100ml = volume / 100.0;
+
+            Console.WriteLine($"{volume}ml {name}:");
+            Console.WriteLine($"{energy * volumePer100ml}kcal, {sugar * volumePer100ml}g sugars");
+        }
+    }
+}
